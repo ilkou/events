98ea46467b749b1f1dc8f98cac65389a6b9e56d9
ilkou <ouklichoussama@gmail.com>
2020-01-11 01:16:54 +0100

    home in progress

--- index.php
+++ index.php
@@ -17,7 +17,11 @@
         <main>
             <?php
                 for ($i = 1; $i <= 3; $i++) {
-                    echo "<section><img src='img/event".$i.".jpg'></section>";
+                    $description = file_get_contents("event_data/event".$i.".txt");
+                    $d_first = substr($description, 0, 50);
+                    $d_second = substr($description, 50, strlen($description));
+                    echo '<section><div><img src="event_data/event'.$i.'.jpg"></div><div class="description"><h1>Event'.$i.'</h1><div>'.$d_first.'<span id="dots'.$i.'">...</span><span id="more'.$i.'">'.$d_second.'</span></div>
+                    <button onclick="hide_text('.$i.')" id="btn'.$i.'">Lire la suite</button></div></section>';
                 }
             ?>
         </main>
@@ -52,6 +56,24 @@
                 document.getElementsByClassName('login-input')[0].value = '';
                 document.getElementsByClassName('login-input')[1].value = '';
             })
+            for (i = 1; i <= 3; i++) {
+                document.getElementById('more'+i.toString()).style.display = 'none';
+                document.getElementById('btn'+i.toString()).style = 'border: none; outline:0; color: blue';
+            }
+            function hide_text(i) {
+                var dots = document.getElementById("dots"+i.toString());
+                var moreText = document.getElementById("more"+i.toString());
+                var btnText = document.getElementById("btn"+i.toString());
+                if (dots.style.display === "none") {
+                    dots.style.display = "inline";
+                    btnText.innerHTML = "Lire la suite";
+                    moreText.style.display = "none";
+                } else {
+                    dots.style.display = "none";
+                    btnText.innerHTML = "Voir moins";
+                    moreText.style.display = "inline";
+                }
+            }
         </script>
     </body>
 </html>
